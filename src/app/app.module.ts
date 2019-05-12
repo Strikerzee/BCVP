cbbeedd88e5ad0892b4679a966bffc245714af29
Arpit Bhadauria <singharpit511@gmail.com>
2019-05-12 12:31:30 +0530

    Revert "Added new branch"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import {ReactiveFormsModule} from '@angular/forms'
-import { SendLoginDataService } from './send-login-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [
-    SendLoginDataService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
