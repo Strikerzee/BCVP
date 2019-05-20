d141061ac3b0f9ec5c5aaf5227e53a6eca5fc2ea
Arnav <B17036@students.iitmandi.ac.in>
2019-05-21 03:22:44 +0530

    xxx

---
+++ BCVP/Hyperledger-Fabric_Arnav/fabric-samples/voting-block/voting-block.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+export VERBOSE=false
+
+function printHelp() {
+  echo "Usage: "
+  echo "  byfn.sh <mode> [-c <channel name>] [-t <timeout>] [-d <delay>] [-f <docker-compose-file>] [-s <dbtype>] [-o <consensus-type>] [-i <imagetag>] [-v]"
+  echo "    <mode> - 'up'"
+  echo "      - 'up' - bring up the network with docker-compose up"
+  echo "    -c <channel name> - channel name to use (defaults to \"voting-channel\")"
+  echo "    -t <timeout> - CLI timeout duration in seconds (defaults to 10)"
+  echo "    -d <delay> - delay duration in seconds (defaults to 3)"
+  echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
+  echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
+  echo "    -o <consensus-type> - the consensus-type of the ordering service: solo (default) or kafka"
+  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo
+  echo "	byfn.sh up"
+}
+
+function checkPrereqs() {
+  LOCAL_VERSION=$(configtxlator version | sed -ne 's/ Version: //p')
+  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/ Version: //p' | head -1)
+
+  echo "LOCAL_VERSION=$LOCAL_VERSION"
+  echo "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
+
+  if [ "$LOCAL_VERSION" != "$DOCKER_IMAGE_VERSION" ]; then
+    echo "=================== WARNING ==================="
+    echo "  Local fabric binaries and docker images are  "
+    echo "  out of  sync. This may cause problems.       "
+    echo "==============================================="
+  fi
+}
+
+function networkUp() {
+  checkPrereqs
+  if [ ! -d "crypto-config" ]; then
+    generateCerts
+    replacePrivateKey
+    generateChannelArtifacts
+  fi
+  if [ "${IF_COUCHDB}" == "couchdb" ]; then
+    if [ "$CONSENSUS_TYPE" == "kafka" ]; then
+      IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_KAFKA -f $COMPOSE_FILE_COUCH up -d 2>&1
+    else
+      IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
+    fi
+  else
+    if [ "$CONSENSUS_TYPE" == "kafka" ]; then
+      IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_KAFKA up -d 2>&1
+    else
+      IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE up -d 2>&1
+    fi
+  fi
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+  fi
+
+  if [ "$CONSENSUS_TYPE" == "kafka" ]; then
+    sleep 1
+    echo "Sleeping 10s to allow kafka cluster to complete booting"
+    sleep 9
+  fi
+
+  docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Test failed"
+    exit 1
+  fi
+}
+
+function replacePrivateKey() {
+  ARCH=$(uname -s | grep Darwin)
+  if [ "$ARCH" == "Darwin" ]; then
+    OPTS="-it"
+  else
+    OPTS="-i"
+  fi
+
+  cp docker-compose-e2e-template.yaml docker-compose-e2e.yaml
+
+  CURRENT_DIR=$PWD
+  cd crypto-config/peerOrganizations/org1.example.com/ca/
+  PRIV_KEY=$(ls *_sk)
+  cd "$CURRENT_DIR"
+  sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+  cd crypto-config/peerOrganizations/org2.example.com/ca/
+  PRIV_KEY=$(ls *_sk)
+  cd "$CURRENT_DIR"
+  sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+  # If MacOSX, remove the temporary backup of the docker-compose file
+  if [ "$ARCH" == "Darwin" ]; then
+    rm docker-compose-e2e.yamlt
+  fi
+}
+
+function generateCerts() {
+  which cryptogen
+  if [ "$?" -ne 0 ]; then
+    echo "cryptogen tool not found. exiting"
+    exit 1
+  fi
+  echo
+  echo "##########################################################"
+  echo "##### Generate certificates using cryptogen tool #########"
+  echo "##########################################################"
+
+  if [ -d "crypto-config" ]; then
+    rm -Rf crypto-config
+  fi
+  set -x
+  cryptogen generate --config=./crypto-config.yaml
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate certificates..."
+    exit 1
+  fi
+  echo
+}
+
+function generateChannelArtifacts() {
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    echo "configtxgen tool not found. exiting"
+    exit 1
+  fi
+
+  echo "##########################################################"
+  echo "#########  Generating Orderer Genesis block ##############"
+  echo "##########################################################"
+  # Note: For some unknown reason (at least for now) the block file can't be
+  # named orderer.genesis.block or the orderer will fail to launch!
+  echo "CONSENSUS_TYPE="$CONSENSUS_TYPE
+  set -x
+  if [ "$CONSENSUS_TYPE" == "solo" ]; then
+    configtxgen -profile TwoOrgsOrdererGenesis -channelID byfn-sys-channel -outputBlock ./channel-artifacts/genesis.block
+  elif [ "$CONSENSUS_TYPE" == "kafka" ]; then
+    configtxgen -profile SampleDevModeKafka -channelID byfn-sys-channel -outputBlock ./channel-artifacts/genesis.block
+  else
+    set +x
+    echo "unrecognized CONSESUS_TYPE='$CONSENSUS_TYPE'. exiting"
+    exit 1
+  fi
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate orderer genesis block..."
+    exit 1
+  fi
+  echo
+  echo "#################################################################"
+  echo "### Generating channel configuration transaction 'channel.tx' ###"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate channel configuration transaction..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Org1MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Org1MSP..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Org2MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate \
+    ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Org2MSP..."
+    exit 1
+  fi
+  echo
+}
+
+# Obtain the OS and Architecture string that will be used to select the correct
+# native binaries for your platform, e.g., darwin-amd64 or linux-amd64
+OS_ARCH=$(echo "$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+# timeout duration - the duration the CLI should wait for a response from
+# another container before giving up
+CLI_TIMEOUT=10
+# default for delay between commands
+CLI_DELAY=3
+# channel name defaults to "voting-channel"
+CHANNEL_NAME="voting-channel"
+# use this as the default docker-compose yaml definition
+COMPOSE_FILE=docker-compose-cli.yaml
+#
+COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+# kafka and zookeeper compose file
+COMPOSE_FILE_KAFKA=docker-compose-kafka.yaml
+
+LANGUAGE=golang
+# default image tag
+IMAGETAG="latest"
+# default consensus type
+CONSENSUS_TYPE="solo"
+MODE=$1
+shift
+
+if [ "$MODE" == "up" ]; then
+  EXPMODE="Starting"
+else
+  printHelp
+  exit 1
+fi
+
+while getopts "h?c:t:d:f:s:i:o:v" opt; do
+  case "$opt" in
+  h | \?)
+    printHelp
+    exit 0
+    ;;
+  c)
+    CHANNEL_NAME=$OPTARG
+    ;;
+  t)
+    CLI_TIMEOUT=$OPTARG
+    ;;
+  d)
+    CLI_DELAY=$OPTARG
+    ;;
+  f)
+    COMPOSE_FILE=$OPTARG
+    ;;
+  s)
+    IF_COUCHDB=$OPTARG
+    ;;
+  i)
+    IMAGETAG=$(go env GOARCH)"-"$OPTARG
+    ;;
+   o)
+    CONSENSUS_TYPE=$OPTARG
+    ;;
+  esac
+done
+
+
+# Announce what was requested
+
+if [ "${IF_COUCHDB}" == "couchdb" ]; then
+  echo
+  echo "${EXPMODE} for channel '${CHANNEL_NAME}' with CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds and using database '${IF_COUCHDB}'"
+else
+  echo "${EXPMODE} for channel '${CHANNEL_NAME}' with CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds"
+fi
+
+#Create the network using docker compose
+if [ "${MODE}" == "up" ]; then
+  networkUp
+else
+  printHelp
+  exit 1
+fi
---
+++ BCVP/Nodejs_Sharad/server/routes/login.js
@@ -0,0 +1,218 @@
+let mysql = require('mysql');
+let router = require('express').Router();
+let bodyparser = require('body-parser');
+const bcrypt = require('bcrypt');
+var saltrounds = 10;
+
+let urlencodedparser = bodyparser.urlencoded({extended: false});
+let jsonencodedparser = bodyparser.json();
+
+router.options('/login', (req, res) => {
+    res.set('Access-Control-Allow-Methods', 'OPTIONS, POST');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.send();
+})
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sampledatabase'
+})
+connection.connect( (error) => {
+    if(!!error)
+    {
+        console.log("Error",error);
+    }
+    else
+    {
+        console.log("Successful Connection"); 
+    }
+});
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+};
+
+function set_token_null(obj,resp)
+{
+    
+    connection.query("update People set token = NULL where VoterID = ?",obj,(err,rows) => {
+        if(err)
+         throw err;
+        else
+         resp.send("token did not match logging out\n");
+    })
+};
+
+function generate_token(obj,resp) {
+    bcrypt.hash(obj,saltrounds,function(err,hash)
+    {
+        if(err)
+        {
+            resp.send("error while generating token\b");
+            throw err;
+            
+        }
+        else
+        {
+            var sql = "update People set token = ? where VoterID = " + obj;
+            connection.query(sql,hash,(error,rows) => {
+                if(error)
+                {
+                    throw error;
+                    
+                }
+                else
+                {
+                    resp.send(hash);
+                }
+            })
+           
+        }
+    })
+}
+
+router.post('/login',function(req,resp){
+    
+    var id =  req.body.id;
+    var password = req.body.password;
+    console.log(password);
+    var sql = "select * from People where VoterID = " +  id;
+    connection.query(sql, (error,rows,fields) => {
+        if(!!error){
+            throw error;
+            
+        }
+        else
+        {
+           if(isEmpty(rows))
+           {
+               console.log("Voter ID does not exist");
+               resp.send("Voter ID does not exist\n");
+           }
+           else
+           {
+                hash = rows[0].Password;
+                bcrypt.compare(password, hash, function(err, res) 
+                    {
+                        if(err)
+                        {
+                            console.log(err);
+                        }    
+                        else
+                        {
+                            if(res ==  true)
+                            {
+                                console.log("User is logged in");
+                                // resp.send("user is logged in\n");
+                                 generate_token(id,resp);                              
+                                
+                            }
+                            else if(res == false)
+                            {
+                                console.log("password is wrong");
+                                resp.send("password is wrong\n");
+                            }
+                        }
+                    });
+            }
+        }
+    });
+});
+
+router.post('/submit',function(req,resp){
+    var id = req.body.id;
+    var token =  req.body.token;
+    var sql2 =  "select token from People where VoterID = ?" ;
+    var sql1 = "update People set Voted = 1 where VoterID = ?";
+    connection.query(sql2,id,(error,rows,fields) => {
+        if(error)
+        {
+            console.log(error);
+        }
+        else if(isEmpty(rows))
+        {
+            console.log("wrong Voter id");
+            resp.send("wrong Voter id\n");
+        }
+        else
+        {
+            var o_token = rows[0].token;
+            if(o_token == null)
+            {
+                resp.send("token did not match logged out\n");
+            }
+            else if( token.trim() == o_token.toString())
+            {
+                connection.query(sql1,id,(error,rows) => {
+                    console.log("user has voted");
+                });
+                console.log("token matched");    
+                generate_token(id,resp);
+            }
+            else
+            {
+                set_token_null(id,resp);
+                console.log("token did not match"); 
+            }
+
+        }
+    });
+})
+
+router.post('/dashboard',function(req,resp){
+    var id = req.body.id;
+    var token =  req.body.token;
+    var sql2 =  "select token from People where VoterID = ?";
+    connection.query(sql2,id,(error,rows,fields) => {
+        if(error)
+        {
+            console.log(error);
+        }
+        else if(isEmpty(rows))
+        {
+            console.log("wrong Voter id");
+            resp.send("wrong Voter id\n");
+        }
+        else
+        {
+            var o_token = rows[0].token;
+            if(o_token == null)
+            {
+                set_token_null(id,resp);
+            }
+            else if(token.trim() == o_token.toString())
+            {
+                console.log("token matched");    
+                generate_token(id,resp);
+            }
+            else
+            {
+                set_token_null(id,resp);
+                console.log("token did not match"); 
+            }
+
+        }
+    });
+})
+
+// router.post('/login', urlencodedparser, (req, res)=>{
+//     console.log('Login request received');
+//     let cred = req.body;
+//     let query = "select username, password from userData where username = " + cred.username + " and password = " + cred.password;
+//     connection.query(query, (err, rows)=>{
+//         if(err)
+//             console.log(err.stack);
+//         else if(rows.length)
+//             res.send('<p> You are logged in! </p>');
+//         else
+//             res.send('<p> Invalid Credentials </p>')
+//     })
+// })
+
+module.exports = router;
